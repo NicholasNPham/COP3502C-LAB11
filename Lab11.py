9e44f0f3cb697a11ecfbf684f65b5ad397643438
NicholasNPham <phamn1000@gmail.com>
2025-11-22 13:35:49 -0500

    stored submissions in dict

--- Lab11.py
+++ Lab11.py
@@ -1,3 +1,5 @@
+import os
+
 file_path_student = r"data\students.txt"
 file_path_assignment = r"data\assignments.txt"
 
@@ -9,7 +11,7 @@ with open(file_path_student, 'r') as file:
         name = line[3:].strip()
         student_data_dict[name] = student_id
 
-print(student_data_dict)
+# print(student_data_dict)
 
 assign_id_to_total_points = {}
 assign_name_to_assign_id = {}
@@ -21,5 +23,23 @@ with open(file_path_assignment, 'r') as file:
         assign_id_to_total_points[lines[i+1].strip()] = lines[i+2].strip()
         assign_name_to_assign_id[lines[i].strip()] = lines[i+1].strip()
 
-print(assign_id_to_total_points)
-print(assign_name_to_assign_id)
+# print(assign_id_to_total_points)
+# print(assign_name_to_assign_id)
+
+submissions_dict = {}
+
+files = os.listdir("data/submissions")
+for filename in files:
+    with open(f"data/submissions/{filename}", 'r') as file:
+        split_info = file.readline().split("|")
+
+        student_id = split_info[0]
+        assignment_id = split_info[1]
+        grade = split_info[2]
+
+        if student_id not in submissions_dict:
+            submissions_dict[student_id] = {}
+
+        submissions_dict[student_id][assignment_id] = grade
+
+print(submissions_dict)
