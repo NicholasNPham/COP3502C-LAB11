204ec57d4937e1f5bb78a1ca04c2088dda69ef9f
NicholasNPham <phamn1000@gmail.com>
2025-11-22 13:38:42 -0500

    gave int functions to numbers

--- Lab11.py
+++ Lab11.py
@@ -7,7 +7,7 @@ student_data_dict = {}
 
 with open(file_path_student, 'r') as file:
     for line in file:
-        student_id = line[0:3]
+        student_id = int(line[0:3])
         name = line[3:].strip()
         student_data_dict[name] = student_id
 
@@ -20,8 +20,8 @@ with open(file_path_assignment, 'r') as file:
     lines = file.readlines()
     for i in range(0, len(lines), 3):
 
-        assign_id_to_total_points[lines[i+1].strip()] = lines[i+2].strip()
-        assign_name_to_assign_id[lines[i].strip()] = lines[i+1].strip()
+        assign_id_to_total_points[int(lines[i+1].strip())] = int(lines[i+2].strip())
+        assign_name_to_assign_id[lines[i].strip()] = int(lines[i+1].strip())
 
 # print(assign_id_to_total_points)
 # print(assign_name_to_assign_id)
@@ -33,13 +33,13 @@ for filename in files:
     with open(f"data/submissions/{filename}", 'r') as file:
         split_info = file.readline().split("|")
 
-        student_id = split_info[0]
-        assignment_id = split_info[1]
-        grade = split_info[2]
+        student_id = int(split_info[0])
+        assignment_id = int(split_info[1])
+        grade = float(split_info[2])
 
         if student_id not in submissions_dict:
             submissions_dict[student_id] = {}
 
         submissions_dict[student_id][assignment_id] = grade
 
-print(submissions_dict)
+# print(submissions_dict)
