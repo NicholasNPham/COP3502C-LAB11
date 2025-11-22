b9a84b0cb105df5611fa0532d402d4f3bb9a20ed
NicholasNPham <phamn1000@gmail.com>
2025-11-22 13:13:28 -0500

    created two new dicts

--- Lab11.py
+++ Lab11.py
@@ -1,4 +1,5 @@
 file_path_student = r"data\students.txt"
+file_path_assignment = r"data\assignments.txt"
 
 student_data_dict = {}
 
@@ -9,3 +10,16 @@ with open(file_path_student, 'r') as file:
         student_data_dict[name] = student_id
 
 print(student_data_dict)
+
+assign_id_to_total_points = {}
+assign_name_to_assign_id = {}
+
+with open(file_path_assignment, 'r') as file:
+    lines = file.readlines()
+    for i in range(0, len(lines), 3):
+
+        assign_id_to_total_points[lines[i+1].strip()] = lines[i+2].strip()
+        assign_name_to_assign_id[lines[i].strip()] = lines[i+1].strip()
+
+print(assign_id_to_total_points)
+print(assign_name_to_assign_id)
